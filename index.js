d12104f58777513c6520a20b86cad05a15684541
Cody Roberts <pithlyx@gmail.com>
2023-05-01 16:13:44 -0600

    completed lab, adding functions and manuipulating arrays

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    let firstTwo = [drivers[0], drivers[1]]
+    return firstTwo
+}
+const returnLastTwoDrivers = (drivers) => {
+    let lastTwo = [drivers[drivers.length-2], drivers[drivers.length-1]]
+    return lastTwo
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(mult){
+    return function (fare){
+        return fare * mult
+    }
+}
+
+const fareDoubler = (fare) => fare*2
+const fareTripler = (fare) => fare*3
+
+function selectDifferentDrivers(arrayOfDrivers, func){
+    return func(arrayOfDrivers)
+}
